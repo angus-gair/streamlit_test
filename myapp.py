1db4060272e3fcbe5c323e4d53b4409d15eabba7
angus-gair <64054686+angus-gair@users.noreply.github.com>
2023-11-24 13:01:53 +1100

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,38 +1,31 @@
 import streamlit as st
 import pandas as pd
-from datetime import datetime, timedelta
+import seaborn as sns
+import numpy as np
 
-# Sample DataFrame
+# Sample DataFrame with random data
+np.random.seed(42)
 data = {
-    'Date': pd.date_range(start='2023-01-01', end='2023-01-10'),
-    'Value': [i for i in range(1, 11)]
+    'Values': np.random.randn(100)
 }
 
 df = pd.DataFrame(data)
 
 # Title for your app
-st.title("Date Range Selector App")
+st.title("Histogram with Line of Best Fit")
 
-# Add a date range selector
-start_date = pd.to_datetime(st.date_input("Select start date", df['Date'].min(), key='start_date'))
-end_date = pd.to_datetime(st.date_input("Select end date", df['Date'].max(), key='end_date'))
+# Sidebar for customization
+st.sidebar.header("Histogram Settings")
+bin_count = st.sidebar.slider("Number of Bins", min_value=1, max_value=100, value=20)
 
-# %%
-#start_date = pd.to_datetime('2023-01-03')
-#end_date = pd.to_datetime('2023-01-05')
+# Create histogram
+st.subheader("Histogram of Values")
+sns.histplot(df['Values'], bins=bin_count, kde=True)
 
-# Filter the DataFrame based on the selected date range
-filtered_df = df[(df['Date'] >= start_date) & (df['Date'] <= end_date)]
+# Calculate and plot the line of best fit
+mean_value = df['Values'].mean()
+std_dev = df['Values'].std()
+x_values = np.linspace(df['Values'].min(), df['Values'].max(), 100)
+y_values = (1 / (std_dev * np.sqrt(2 * np.pi))) * np.exp(-0.5 * ((x_values - mean_value) / std_dev)**2)
 
-# Display the selected date range
-st.write("Selected Date Range:", start_date, "to", end_date)
-
-# Display the filtered DataFrame
-st.write("Filtered DataFrame:")
-st.write(filtered_df)
-
-
-min_date = datetime.date(df['Date'].min())
-max_date = datetime.date(df['Date'].max())
-
-st.slider("Select a date range", min_value=min_date, max_value=max_date, value=(start_date, end_date), step=timedelta(days=1),label_visibility="visible")
+st.line_chart(pd.DataFrame({'X': x_values, 'Y': y_values}), use_container_width=True)
